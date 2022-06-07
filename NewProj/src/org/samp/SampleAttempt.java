a8ce55b9636b73b3d366596387b3d694a889333d
DiViMa2021 <rmanieee09@gmail.com>
2022-06-07 23:19:41 +0530

    attempt

---
+++ NewProj/src/org/samp/SampleAttempt.java
@@ -0,0 +1,17 @@
+package org.samp;
+
+public class SampleAttempt {
+	
+	private void first() {
+		System.out.println("Hi...");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		SampleAttempt a=new SampleAttempt();
+		
+		a.first();
+	}
+
+}
